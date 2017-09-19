c5de607b448b9d7aa43e2223bc4d5ed475ceaae0
Lee <kyeongyong1@github.com>
2017-09-19 23:16:05 +0900

    Initial Commit

---
+++ PatternStateTests/PatternStateTests.swift
@@ -0,0 +1,36 @@
+//
+//  PatternStateTests.swift
+//  PatternStateTests
+//
+//  Created by Lee on 2017. 9. 19..
+//  Copyright © 2017년 LEE. All rights reserved.
+//
+
+import XCTest
+@testable import PatternState
+
+class PatternStateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
